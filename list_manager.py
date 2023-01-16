f92d860c8c08ec68541bad19040be17e7ea1df59
jonasrorschach <jonas.b.m@outlook.com>
2023-01-16 16:32:09 -0300

    1.1

--- list_manager.py
+++ list_manager.py
@@ -56,66 +56,69 @@ while True:
        
         elif opcao.upper() == 'A':
 
-            if any_lista:
+            if any_lista: # se houver algo dentro da lista vai rodar
                 apagar = input('Quantos itens serão apagados da lista? ')
+                apagar_num = apagar.isnumeric() # se apagar for número será truthy
+
+        
+            else:       # se não houver nada dentro da lista vai rodar
+                print('Não há nada dentro da lista para ser apagado.') 
+                sleep(3)
+                break
             
-            apagar_num = apagar.isnumeric()
+            # try: # coloquei try caso any_lista e apagar_num seja truthy
 
             if apagar_num:
                 apagar_int = int(apagar)
-                
                 tamanho_toleravel = len(lista) >= apagar_int
                 tamanho_nao_toleravel = len(lista) < apagar_int
-
+                
                 if tamanho_toleravel:
                     # · Vou ter que ver qual é o número do iterator dentro da lista para passar para apagar
                     # · A quantidade de itens tem que ser certa, se não o limitador pergunta
-                    itens = input('Cite exatamente nome dos itens que serão apagados da lista, com separação por espaço: ').rstrip().split()
-
-                    for i in range(0, len(itens) - 1):
-                        num_item = lista.index(item[i])
-                        lista.remove(num_item)
+                    try:
+                        
+                        for i in range(0, apagar_int):
+                            iter_apagar = input(f'Insira o nome do {i + 1}º que deve ser apagado: ')
+                            lista.remove(iter_apagar)
+                        print('Todos itens selecionados foram removidos')
+                        syscln(2)
+                        break
                     
-                    print('Todos itens selecionados foram removidos')
-                    syscln(3)
-                    break
+                    except:
+                        print('O nome do objeto a ser apagador foi digitado errado. Tente novamente.')
+
 
                 elif tamanho_nao_toleravel:
                     print('O número que você digitou é maior do que o número de itens dentro da lista. Tente novamente.')
-                    syscln(3)
-                    break
-
-
-                else:
-                    print('Não há nada dentro da lista para ser apagado.')
-                    sleep(3)
+                    syscln(2)
                     break
             
             else:
-                print(f'Só é permitido a inserção de números dentro da quantidade de itens na lista, no momento com {len(lista)} itens.')
-                sleep(2)
+                print('O valor digitado não é um número. Tente novamente.')
                 break
-                
 
 
+            
 
+            # except:
+            #     ...
+            
 
 
         elif opcao.upper() == 'L':
             
             if any_lista:
                 print(f'Esses são todos os itens da sua lista: {lista}')
+                syscln(2)
 
                 msg = 'Fim da lista'
-
-                for i in range(0, len(lista) + 1):
-                    print(f'\t {lista[i]}')
                 
                 for j in range(0, 3):
                     print(msg)
                     msg += '.'
                     syscln(1)
-                    break
+                break
 
 
 
@@ -125,6 +128,7 @@ while True:
             
         else:
             print('Por favor apenas insira uma letra de acordo com o comando.')
+
 
 
     question = input('Quer fazer mais alguma alteração?[S]im ou [N]ão ')
@@ -140,3 +144,6 @@ while True:
             print(msg)
             msg += '.'
         break
+    
+    else:
+        print('Só é permitido "S" para "sim" e "N" para "não". Tente novamente. ')
